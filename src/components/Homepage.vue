1eb421d763f63897b2e8d5ba178938599e755e2a
Umoren <samuelumoren365@gmail.com>
2020-07-21 12:52:47 +0100

    Initial commit

---
+++ src/components/Homepage.vue
@@ -0,0 +1,21 @@
+<template>
+ <Layout>
+       <apicart-category class="category" />
+ </Layout>
+</template>
+
+<script>
+import { ApicartCategory } from '~/assets/js/app.js';
+
+export default {
+   components: {
+        ApicartCategory
+   },
+   metaInfo: {
+       title: 'Category',
+       meta: [
+           { key: 'description', name: 'description', content: 'Amazing e-commerce!' }
+       ]
+   }
+}
+</script>
